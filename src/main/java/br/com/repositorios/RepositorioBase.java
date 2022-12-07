a3a54b75c449c8766b84aac6715abe14ac0ccca0
ViniciusSugawara <vinicius.sugawara@gmail.com>
2022-12-07 14:44:59 -0300

    feat: added RepositorioBase

---
+++ src/main/java/br/com/repositorios/RepositorioBase.java
@@ -0,0 +1,12 @@
+package br.com.repositorios;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.List;
+@NoRepositoryBean
+public interface RepositorioBase<T, ID>{
+    void save (T object);
+    List<T> findAll();
+    T findById(ID id);
+    void delete(T object);
+    void deleteById(ID id);
+}
