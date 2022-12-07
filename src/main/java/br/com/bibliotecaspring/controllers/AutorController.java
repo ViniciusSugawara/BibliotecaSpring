8b4436fbd8aab4250008cf0fb93e2a7ce27090a4
ViniciusSugawara <vinicius.sugawara@gmail.com>
2022-12-07 17:30:15 -0300

    feat: added AutorController

---
+++ src/main/java/br/com/bibliotecaspring/controllers/AutorController.java
@@ -0,0 +1,47 @@
+package br.com.bibliotecaspring.controllers;
+
+import br.com.bibliotecaspring.models.Autor;
+import br.com.bibliotecaspring.servicos.CrudServico;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Qualifier("AutorController")
+public class AutorController {
+
+    private CrudServico autorServico;
+
+    public AutorController(@Qualifier("Autor") CrudServico autorServico){
+        this.autorServico = autorServico;
+    }
+
+    @RequestMapping({"/achaAutores"})
+    public Autor findById(Long id){
+        return (Autor) autorServico.findById(id);
+    }
+
+    @RequestMapping("/testeApi")
+    @GetMapping
+    public Autor retornaAutor(){
+        Autor autorMock = new Autor();
+        autorMock.setNome("Joj");
+        return autorMock;
+    }
+
+    @RequestMapping("/achaTodos")
+    public List<Autor> findAll(){
+        return (List<Autor>) autorServico.findAll();
+    }
+
+    @RequestMapping("/insereAutor")
+    @PostMapping
+    public void save(Autor object){
+        this.autorServico.save(object);
+    }
+
+}
