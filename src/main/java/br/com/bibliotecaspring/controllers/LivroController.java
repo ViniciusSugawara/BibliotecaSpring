b2151d69478a7719170c96b8bdd3c47956f74bba
ViniciusSugawara <vinicius.sugawara@gmail.com>
2022-12-13 17:13:06 -0300

    feat: added LivroController methods and maps

--- src/main/java/br/com/bibliotecaspring/controllers/LivroController.java
+++ src/main/java/br/com/bibliotecaspring/controllers/LivroController.java
@@ -1,7 +1,10 @@
 package br.com.bibliotecaspring.controllers;
 
-import br.com.bibliotecaspring.repositorios.LivroRepositorio;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.bibliotecaspring.models.Livro;
+import br.com.bibliotecaspring.servicos.LivroServico;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class LivroController {
@@ -11,7 +14,40 @@ public class LivroController {
         this.livroServico = livroServico;
     }
 
-    public LivroController(LivroRepositorio livroRepositorio){
-        this.livroRepositorio = livroRepositorio;
+    @RequestMapping("/index")
+    @GetMapping
+    public List<Livro> findAll(){
+        return livroServico.findAll();
+    }
+
+    @RequestMapping({"/findById"})
+    @GetMapping
+    public Livro findById(Long id){
+        return livroServico.findById(id);
+    }
+
+    @RequestMapping("/returnMock")
+    @GetMapping
+    public Livro retornaLivro(){
+        Livro LivroMock = new Livro();
+        LivroMock.setNome("AErfj9-0wossfkweo0");
+        return LivroMock;
+    }
+
+    @RequestMapping("/store")
+    @PostMapping
+    public void save(Livro object){
+        this.livroServico.save(object);
+    }
+    @RequestMapping("/delete")
+    @DeleteMapping
+    public void delete(Livro object){
+        this.livroServico.delete(object);
+    }
+
+    @RequestMapping("/deleteById")
+    @DeleteMapping
+    public void deleteById(Long id){
+        this.livroServico.deleteById(id);
     }
 }
