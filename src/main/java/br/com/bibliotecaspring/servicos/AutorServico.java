97387fa06654557aa8e3582ab12c8bbd5c671f72
ViniciusSugawara <vinicius.sugawara@gmail.com>
2022-12-13 17:09:29 -0300

    refactor: removed CrudServico implementation in AutorServico and removed Override annotations

--- src/main/java/br/com/bibliotecaspring/servicos/AutorServico.java
+++ src/main/java/br/com/bibliotecaspring/servicos/AutorServico.java
@@ -3,7 +3,6 @@ package br.com.bibliotecaspring.servicos;
 import br.com.bibliotecaspring.models.Autor;
 import br.com.bibliotecaspring.models.Livro;
 import br.com.bibliotecaspring.repositorios.AutorRepositorio;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -11,38 +10,33 @@ import java.util.List;
 
 @Qualifier("Autor")
 @Service
-public class AutorServico implements CrudServico<Autor, Long> {
+public class AutorServico {
     private AutorRepositorio autorRepositorio;
-    public AutorServico(@Qualifier("AutorRepo") AutorRepositorio autorRepositorio){
+    public AutorServico(AutorRepositorio autorRepositorio){
         this.autorRepositorio = autorRepositorio;
     }
-    @Override
     public List<Autor> findAll() {
         return autorRepositorio.findAll();
     }
-
-    @Override
     public Autor findById(Long id) {
         return autorRepositorio.findById(id);
     }
-    public List<Livro> findAllLivros(){ return autorRepositorio.findAllLivros(); }
 
-    @Override
     public void save(Autor object) {
         autorRepositorio.save(object);
     }
 
-    @Override
+
     public void delete(Autor object) {
         autorRepositorio.delete(object);
     }
 
-    @Override
+
     public void deleteById(Long id) {
         autorRepositorio.deleteById(id);
     }
 
-    @Override
+
     public void updateById(Long id, Autor object) {
         Autor autor = this.findById(id);
         autor = object;
