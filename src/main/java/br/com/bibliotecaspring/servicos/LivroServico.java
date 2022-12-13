2f02af544987edbceecbd63541ee965f149aa41a
ViniciusSugawara <vinicius.sugawara@gmail.com>
2022-12-13 17:15:55 -0300

    refactor: removed CrudServico implementation in LivroServico and removed Override annotations

--- src/main/java/br/com/bibliotecaspring/servicos/LivroServico.java
+++ src/main/java/br/com/bibliotecaspring/servicos/LivroServico.java
@@ -9,38 +9,34 @@ import java.util.List;
 
 @Qualifier("Livro")
 @Service
-public class LivroServico implements CrudServico<Livro, Long> {
+public class LivroServico {
     private LivroRepositorio livroRepositorio;
 
-    public LivroServico(@Qualifier("LivroRepo") LivroRepositorio livroRepositorio){
+    public LivroServico(LivroRepositorio livroRepositorio){
         this.livroRepositorio = livroRepositorio;
     }
-    @Override
+
     public List<Livro> findAll() {
         return livroRepositorio.findAll();
     }
 
-    @Override
     public Livro findById(Long id) {
         return livroRepositorio.findById(id);
     }
 
-    @Override
     public void save(Livro object) {
         livroRepositorio.save(object);
     }
 
-    @Override
+
     public void delete(Livro object) {
         livroRepositorio.delete(object);
     }
 
-    @Override
     public void deleteById(Long id) {
         livroRepositorio.deleteById(id);
     }
 
-    @Override
     public void updateById(Long id, Livro object) {
         Livro livro = findById(id);
         livro = object;
