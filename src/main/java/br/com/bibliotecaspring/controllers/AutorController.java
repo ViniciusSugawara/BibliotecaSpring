d758ed52334901563c6678c94c37c82596ef8a22
Vinicius Sugawara <vinicius.sugawara@gmail.com>
2022-12-14 20:56:23 -0300

    feat: added get request findAllLivrosById method on AutorController

--- src/main/java/br/com/bibliotecaspring/controllers/AutorController.java
+++ src/main/java/br/com/bibliotecaspring/controllers/AutorController.java
@@ -2,10 +2,12 @@ package br.com.bibliotecaspring.controllers;
 
 import br.com.bibliotecaspring.dto.AutorDTO;
 import br.com.bibliotecaspring.models.Autor;
+import br.com.bibliotecaspring.models.Livro;
 import br.com.bibliotecaspring.servicos.AutorServico;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/autor")
@@ -28,6 +30,8 @@ public class AutorController {
     public AutorDTO findById(@PathVariable("id") Long id){
         return autorServico.findById(id);
     }
+    @GetMapping("/index/{id}/livros")
+    public Set<Livro> findAllLivrosById(@PathVariable("id") Long id){ return autorServico.findAllLivrosById(id);}
 
     @RequestMapping("/returnMock")
     @GetMapping
