e524f3d1e84af8d7c3fd23ae1975102ffe95d6d4
Vinicius Sugawara <vinicius.sugawara@gmail.com>
2022-12-14 17:44:20 -0300

    refactor: AutorServico now uses AutorDTO(used Autor before)

--- src/main/java/br/com/bibliotecaspring/servicos/AutorServico.java
+++ src/main/java/br/com/bibliotecaspring/servicos/AutorServico.java
@@ -1,5 +1,7 @@
 package br.com.bibliotecaspring.servicos;
 
+import br.com.bibliotecaspring.dto.AutorDTO;
+import br.com.bibliotecaspring.dto.LivroDTO;
 import br.com.bibliotecaspring.models.Autor;
 import br.com.bibliotecaspring.models.Livro;
 import br.com.bibliotecaspring.repositorios.AutorRepositorio;
@@ -18,36 +20,16 @@ public class AutorServico {
     public AutorServico(AutorRepositorio autorRepositorio){
         this.autorRepositorio = autorRepositorio;
     }
-    public List<Autor> findAll() {
-        List<Autor> listaFiltrada = new ArrayList<>();
+    public List<AutorDTO> findAll() {
+        List<AutorDTO> listaFiltrada = new ArrayList<>();
         for(Autor autor : autorRepositorio.findAll()){
             listaFiltrada.add(filterAutor(autor));
         }
         return listaFiltrada;
     }
-    private Autor filterAutor(Autor autor){
-        Set<Livro> livrosFiltrados = new HashSet<>();
-        Autor prototipo = new Autor();
-        prototipo.setId(autor.getId());
-        prototipo.setNome(autor.getNome());
 
-        for(Livro livro : autor.getLivros()){
-            livrosFiltrados.add(filterSetLivros(livro));
-        }
-        prototipo.setLivros(livrosFiltrados);
-        return prototipo;
-    }
-    // Metodo criado para que nao seja retornado um conjunto redundante de autor, que eh lido dentro do JSON
-    // causando erro de stack overflow.
-    private Livro filterSetLivros(Livro livro){
-        Livro livroPrototipo = new Livro();
-        livroPrototipo.setId(livro.getId());
-        livroPrototipo.setNome(livro.getNome());
-        livroPrototipo.setIsbn(livro.getIsbn());
-        return livroPrototipo;
-    }
-    public Autor findById(Long id) {
-        return autorRepositorio.findById(id).orElse(null);
+    public AutorDTO findById(Long id) {
+        return filterAutor(autorRepositorio.findById(id).orElse(null));
     }
 
     public void save(Autor object) {
