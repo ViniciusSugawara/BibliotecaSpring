3b255f15d49a76b1d3a7f56ba98040470608d645
ViniciusSugawara <vinicius.sugawara@gmail.com>
2023-01-04 22:20:11 -0300

    refactor: added Repository annotation in repositories and removed RepositorioBase.

--- src/main/java/br/com/bibliotecaspring/repositorios/AutorRepositorio.java
+++ src/main/java/br/com/bibliotecaspring/repositorios/AutorRepositorio.java
@@ -2,6 +2,8 @@ package br.com.bibliotecaspring.repositorios;
 
 import br.com.bibliotecaspring.models.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AutorRepositorio extends JpaRepository<Autor, Long> {
 }
--- src/main/java/br/com/bibliotecaspring/repositorios/LivroRepositorio.java
+++ src/main/java/br/com/bibliotecaspring/repositorios/LivroRepositorio.java
@@ -2,6 +2,8 @@ package br.com.bibliotecaspring.repositorios;
 
 import br.com.bibliotecaspring.models.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LivroRepositorio extends JpaRepository<Livro, Long> {
 }
